ef4967fbe40783b71d47464edda432955c2452fe
bhushandeore1 <pbhushandeore@gmail.com>
2023-07-03 00:54:05 +0530

    other way

--- BubbleSort/src/bhushan/MergeSortedArray.java
+++ BubbleSort/src/bhushan/MergeSortedArray.java
@@ -14,11 +14,24 @@ public class MergeSortedArray {
 	public static void main(String[] args) {
 		int [] nums1= {1,2,3,0,0,0};
 		int [] nums2= {2,5,6};
+		
+		merge1(nums1,3,nums2,3);
+		
 //		merge(nums1,3,nums2,3);
-		int[] newarr=removezero(nums1);
-		merge(newarr, 3, nums2, 3);
+//		int[] newarr=removezero(nums1);
+//		merge(newarr, 3, nums2, 3);
 	}
 	
+	private static void merge1(int[] nums1, int m, int[] nums2, int n) {
+		for (int j = 0, i = m; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+        System.out.println(Arrays.toString(nums1));
+		
+	}
+
 	public static int[] removezero(int[] nums) {
 		String ans;
 		int count=0;
