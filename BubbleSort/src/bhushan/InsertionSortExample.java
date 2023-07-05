1587fded74bd40dd5b4bc9a27d6cb7e7e15210af
bhushandeore1 <pbhushandeore@gmail.com>
2023-07-06 02:05:45 +0530

    Insertion and selection sort example

---
+++ BubbleSort/src/bhushan/InsertionSortExample.java
@@ -0,0 +1,31 @@
+package bhushan;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class InsertionSortExample {
+
+	public static void main(String[] args) {
+		int []num= {3,5,1,7,2,4};
+		insertionSort(num);
+		System.out.println(Arrays.toString(num));
+
+	}
+
+	private static void insertionSort(int[] num) {
+		for (int i = 0; i < num.length-1; i++) {
+			for (int j = i+1; j >0; j--) {
+				if(num[j-1]>num[j]) {
+					int tem=num[j];
+					num[j]=num[j-1];
+					num[j-1]=tem;
+				}
+				else {
+					break;
+				}
+			}
+		}
+		
+	}
+
+}
---
+++ BubbleSort/src/bhushan/SelectionSortExample.java
@@ -0,0 +1,42 @@
+package bhushan;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class SelectionSortExample {
+
+	public static void main(String[] args) {
+		int []num= {3,5,1,6,2};
+		SelectionSort(num);
+		System.out.println(Arrays.toString(num));
+
+	}
+	
+	private static void SelectionSort(int[] num) {
+		for (int i = 0; i < num.length; i++) {
+			int last=num.length-i-1;
+			int maxnum=getMaxElement(num,0,last);
+			swap(num,maxnum,last);
+		}
+
+	}
+	
+	private static void swap(int[] num, int maxnum, int last) {
+		int tem=num[maxnum];
+		num[maxnum]=num[last];
+		num[last]=tem;
+		
+	}
+
+	private static int  getMaxElement(int []arr,int start, int last) {
+		int max=start;
+		for (int i = start; i <= last; i++) {
+			if(arr[i]>arr[start]) {
+				max=i;
+			}
+		}
+		return max;
+	}
+
+	
+}
